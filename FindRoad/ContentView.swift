b16711bd655d0a4fea2d79e5be9dc796acd202bf
Yermek <tisobyn@gmail.com>
2020-08-30 05:15:44 +0600

    Initial Commit

---
+++ FindRoad/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FindRoad
+//
+//  Created by Yermek Sabyrzhan on 8/30/20.
+//  Copyright © 2020 Yermek Sabyrzhan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
